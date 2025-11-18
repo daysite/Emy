8a47e2fffaef0e54738dae8229e50ea485226ea6
daysite <danieldevelop3@gmail.com>
2025-11-18 11:47:28 -0500

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -2,25 +2,13 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 let creadorID = '5351524614@s.whatsapp.net'
 let isInGroup = m.isGroup && (await conn.groupMetadata(m.chat)).participants.some(p => p.id === creadorID)
 
-let numeroTexto = isInGroup ? `@${creadorID.split('@')[0]}` : `+53 51524614`
+let numeroTexto = isInGroup ? `@${creadorID.split('@')[0]}` : `5493884539290`
 
 let creador = `🌹 *C R E A D O R - 💎 - B O T*
 
-🌱 *NOMBRE:* ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆̟(◣_◢)凸
+🌱 *NOMBRE:* Daniel.qsy
 🍟 *NUMERO:* ${numeroTexto}
-🪴 *LINK:* wa.me/5351524614
-
-👑 *E N L A C E S - U T I L E S:*
-
-• *GRUPO OFC:*
-https://chat.whatsapp.com/F4QEFF2Hn4102NdbPJ2ZOi
-• *CANAL OFC*
-https://whatsapp.com/channel/0029VaqAtuIK0IBsHYXtvA3e
-• *GITHUB:*
-https://github.com/David-Chian
-• *YOUTUBE:*
-https://youtube.com/@davidchian4957
-`
+🪴 *LINK:* wa.me/+5493884539290`
 
 await conn.sendMessage(m.chat, {
   text: creador.trim(),
