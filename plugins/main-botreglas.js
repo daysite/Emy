7c724525fbe6de668d304ccc719021c6601895da
daysite <danieldevelop3@gmail.com>
2025-11-18 13:35:10 -0500

    Update main-botreglas.js

--- plugins/main-botreglas.js
+++ plugins/main-botreglas.js
@@ -2,15 +2,15 @@ let handler = async (m, { conn, usedPrefix, command}) => {
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-let megu = `🚩 *Respeta las reglas de Megumin-Bot-MD*\n
+let megu = `🚩 *Respeta las reglas de Aneka*\n
 ✰ No llamar Al Bot
 ✰ No hacer spam
 ✰ Contacta al creador si es necesario
-✰ Pedir permiso para añadir al bot a un grupo
+✰ realiza primero la compra antes de solicitar que se una el bot
 
-🍬 𝗡𝗼𝘁𝗮: *Si no cumples con las reglas del bot, seras bloqueado.*
+🍬 𝗡𝗼𝘁𝗮: *Si no cumples con las reglas, seras bloqueado.*
 
-⚠️ 𝗔𝘃𝗶𝘀𝗼: *Puedes apoyarnos dejando una estrellita 🌟 al repositorio oficial de Megumin-Bot-MD.*
+⚠️ 𝗔𝘃𝗶𝘀𝗼: *Puedes probar las funciones del bot en el grupo oficial.*
 
 ${global.md}`.trim()
 await conn.reply(m.chat, megu, m, fake)
